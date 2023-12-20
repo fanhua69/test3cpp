b0a9c9372934a7ce1a6a18502ff8964fb46a6e3f
fanhua <fanhua69@gmail.com>
2023-12-20 12:30:43 -0500

    Initial Commit

---
+++ testcpp3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  testcpp3
+//
+//  Created by hua fan on 2023-12-20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
